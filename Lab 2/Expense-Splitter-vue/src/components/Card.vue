f782b2a1f4478207f2c85f6f2b05bc9f2704e792
SMorfin <sdmorfin53@gmail.com>
2026-02-08 12:02:39 -0600

    Added Card & Changed Name

---
+++ Lab 2/Expense-Splitter-vue/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="card">
+        <slot />
+    </main>
+</template>
+
+<style scoped>
+.card {
+    width: min (600px, 100%);
+    background: #a8e4a0;
+    padding: 16px;
+    box-shadow: 0 10px 24px rgb(4, 41, 3);
+}
+</style>
