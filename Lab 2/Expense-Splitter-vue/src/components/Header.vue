36b6bb4370bdea337d6bcd8c5faf47a37aeb926d
SMorfin <sdmorfin53@gmail.com>
2026-02-08 11:51:27 -0600

    Added Header

---
+++ Lab 2/Expense-Splitter-vue/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+   <header class="Top">
+        <h1>Expense Splitter</h1>
+        <p>Split expenses and see who owes what.</p>
+    </header>
+</template>
+
+<style scoped>
+.Top {
+    text-align: center;
+    margin-bottom: 16px;
+}
+
+.Top h1 {
+    margin: 0;
+}
+
+.Top p {
+    margin: 4px 0 0 0;
+    opacity: 0.7;
+}
+</style>
